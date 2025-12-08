caa162dcbc52ad4d1b4f40e6508606a688a5a2cd
svshastry <svshastry@gmail.com>
2025-12-08 05:56:56 +0530

    NumPy 2.x compatibility patch (recfromcsv → genfromtxt) and clean source

---
+++ README.md
@@ -0,0 +1,18 @@
+\# PyPnF (NumPy 2.x Compatible Fork)
+
+
+
+This is a maintained fork of the original PyPI `pypnf` package with
+
+NumPy 2.x compatibility restored.
+
+
+
+\## Install
+
+
+
+pip install git+https://github.com/svshastry/pypnf.git
+
+
+
---
+++ pypnf/testdata.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# pyPnF
+# A Package for Point and Figure Charting
+# https://github.com/swaschke/pypnf
+#
+# Copyright (C) 2021  Stefan Waschke
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from importlib.resources import files
+
+"""
+The testdata module contains different sets of testdata.
+"""
+
+import numpy as np
+
+def recfromcsv(*args, **kwargs):
+    """
+    NumPy 2.x compatible replacement for the removed numpy.recfromcsv
+    """
+    return np.genfromtxt(
+        *args,
+        delimiter=",",
+        names=True,
+        dtype=None,
+        encoding="utf-8"
+    )
+
+
+def dataset(set):
+    """
+    testdata
+    """
+
+    if set == 'AAPL' or 'MSFT' or '^SPX':
+
+        file = 'data/' + set + '.csv'
+
+        path = files('pypnf').joinpath(file)
+        data = recfromcsv(path, encoding='utf-8')
+
+        ts = {'date': [],
+              'open': [],
+              'high': [],
+              'low':  [],
+              'close': [],
+              'volume': []}
+
+        for row in data:
+            ts['date'].append(row[0].astype(str))
+            ts['open'].append(row[1])
+            ts['high'].append(row[2])
+            ts['low'].append(row[3])
+            ts['close'].append(row[4])
+            ts['volume'].append(row[5])
+
+    else:
+
+        raise ValueError("Valid datasets are 'AAPL' and MSFT' and '^SPX'." )
+
+    return ts
